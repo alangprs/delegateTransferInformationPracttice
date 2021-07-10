03995e7c424ae91ce3f2ef08bc88d333addfdd1b
will <alangprs1@gmail.com>
2021-07-10 13:27:44 +0800

    Initial Commit

---
+++ 代理人傳資料練習/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  代理人傳資料練習
+//
+//  Created by 翁燮羽 on 2021/7/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
